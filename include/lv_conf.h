6d3ba7ff5f1e5e6a1ccb5d0dbe252276f6883956
Paul Melnikov <positron96@gmail.com>
2025-03-20 00:47:25 +0100

    Add cyrillic support

--- include/lv_conf.h
+++ include/lv_conf.h
@@ -405,10 +405,11 @@
 /*Optionally declare custom fonts here.
  *You can use these fonts as default font too and they will be available globally.
  *E.g. #define LV_FONT_CUSTOM_DECLARE   LV_FONT_DECLARE(my_font_1) LV_FONT_DECLARE(my_font_2)*/
-#define LV_FONT_CUSTOM_DECLARE
+#define LV_FONT_CUSTOM_DECLARE  LV_FONT_DECLARE(montserrat_cyr_14)
 
 /*Always set a default font*/
-#define LV_FONT_DEFAULT &lv_font_montserrat_14
+#define LV_FONT_DEFAULT  &montserrat_cyr_14
+//&lv_font_montserrat_14
 
 /*Enable handling large font and/or fonts with a lot of characters.
  *The limit depends on the font size, font face and bpp.
